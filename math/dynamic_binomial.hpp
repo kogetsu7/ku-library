700992d2ce7ab706d886e0213ca79e12f8abf413
kogetsu7 <kogetsu0728@gmail.com>
2024-12-01 09:56:50 +0900

    change: unsigned

--- math/dynamic_binomial.hpp
+++ math/dynamic_binomial.hpp
@@ -10,57 +10,55 @@ namespace Ku {
  */
 template <class T> class DynamicBinomial {
   private:
-    int n;
+    unsigned n;
     std::vector<T> fact;
     std::vector<T> ifact;
 
   public:
-    DynamicBinomial() : DynamicBinomial{0} {}
-    explicit DynamicBinomial(const int _n)
+    DynamicBinomial() : DynamicBinomial{0U} {}
+    explicit DynamicBinomial(const unsigned _n)
         : n(_n), fact(_n + 1), ifact(_n + 1) {
-        assert(0 <= n);
-
         fact[0] = T{1};
-        for (int i = 0; i < n; i++) {
+        for (unsigned i = 0; i < n; i++) {
             fact[i + 1] = fact[i] * T{i + 1};
         }
 
-        ifact[n] = T{1} / fact[n];
+        ifact[n] = T{1U} / fact[n];
 
-        for (int i = n; i > 0; i--) {
+        for (unsigned i = n; i > 0; i--) {
             ifact[i - 1] = ifact[i] * T{i};
         }
     }
 
     T p(const int a, const int b) const {
         if (b < 0 || a < b) {
-            return T{0};
+            return T{0U};
         }
 
         assert(0 <= a);
-        assert(a <= n);
-        assert(a - b <= n);
+        assert(a <= static_cast<int>(n));
+        assert(a - b <= static_cast<int>(n));
 
         return fact[a] * ifact[a - b];
     }
 
     T c(const int a, const int b) const {
         if (b < 0 || a < b) {
-            return T{0};
+            return T{0U};
         }
 
-        assert(b <= n);
+        assert(b <= static_cast<int>(n));
 
         return p(a, b) * ifact[b];
     }
 
     T h(const int a, const int b) const {
         if (a == 0 && b == 0) {
-            return T{1};
+            return T{1U};
         }
 
         if (a <= 0 || b < 0) {
-            return T{0};
+            return T{0U};
         }
 
         return c(a + b - 1, b);
--- math/static_binomial.hpp
+++ math/static_binomial.hpp
@@ -8,56 +8,54 @@ namespace Ku {
 /**
  * @brief Static Binomial (二項係数, constexpr)
  */
-template <class T, int N> class StaticBinomial {
-    static_assert(0 < N, "N must be positive.");
-
+template <class T, unsigned N> class StaticBinomial {
   private:
     std::array<T, N + 1> fact;
     std::array<T, N + 1> ifact;
 
   public:
-    constexpr StaticBinomial() {
-        fact[0] = T{1};
-        for (int i = 0; i < N; i++) {
+    constexpr StaticBinomial() : fact{}, ifact{} {
+        fact[0] = T{1U};
+        for (unsigned i = 0; i < N; i++) {
             fact[i + 1] = fact[i] * T{i + 1};
         }
 
-        ifact[N] = T{1} / fact[N];
+        ifact[N] = T{1U} / fact[N];
 
-        for (int i = N; i > 0; i--) {
+        for (unsigned i = N; i > 0; i--) {
             ifact[i - 1] = ifact[i] * T{i};
         }
     }
 
     T p(const int a, const int b) const {
         if (b < 0 || a < b) {
-            return T{0};
+            return T{0U};
         }
 
         assert(0 <= a);
-        assert(a <= N);
-        assert(a - b <= N);
+        assert(a <= static_cast<int>(N));
+        assert(a - b <= static_cast<int>(N));
 
         return fact[a] * ifact[a - b];
     }
 
     T c(const int a, const int b) const {
         if (b < 0 || a < b) {
-            return T{0};
+            return T{0U};
         }
 
-        assert(b <= N);
+        assert(b <= static_cast<int>(N));
 
         return p(a, b) * ifact[b];
     }
 
     T h(const int a, const int b) const {
         if (a == 0 && b == 0) {
-            return T{1};
+            return T{1U};
         }
 
         if (a <= 0 || b < 0) {
-            return T{0};
+            return T{0U};
         }
 
         return c(a + b - 1, b);
