8b9e12ff47245064dc25c39ec2f042266e6d444f
kogetsu <kogetsu0728@gmail.com>
2024-12-08 15:50:10 +0900

    add: note

--- math/extended_gcd.hpp
+++ math/extended_gcd.hpp
@@ -5,6 +5,7 @@
 namespace ku {
 /**
  * @brief Extended Euclidean Algorithm (拡張ユークリッドの互除法)
+ * @note 参考：https://qiita.com/drken/items/b97ff231e43bce50199a
  */
 template <class T> T ExtendedGCD(T a, T b, T& x, T& y) {
     static_assert(!std::is_floating_point_v<T>, "T must not be floating point");
