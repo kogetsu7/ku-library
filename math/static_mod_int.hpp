493aba124e01fdf9f0690c14394708afd4f8b897
kogetsu <kogetsu0728@gmail.com>
2024-12-08 11:39:25 +0900

    add: noexcept

--- math/static_mod_int.hpp
+++ math/static_mod_int.hpp
@@ -11,25 +11,32 @@ template <unsigned M> class StaticModInt {
     unsigned v;
 
   public:
-    constexpr StaticModInt() : StaticModInt(0) {}
-    constexpr explicit StaticModInt(const unsigned long long _v)
+    constexpr StaticModInt() noexcept : v(0) {}
+    constexpr explicit StaticModInt(const unsigned long long _v) noexcept
         : v(static_cast<unsigned>(_v % M)) {}
 
-    constexpr static unsigned mod() { return M; }
+    constexpr static StaticModInt raw(const unsigned _v) noexcept {
+        StaticModInt res;
+        res.v = _v;
 
-    constexpr unsigned val() const { return v; }
+        return res;
+    }
+
+    constexpr static unsigned mod() noexcept { return M; }
+
+    constexpr unsigned val() const noexcept { return v; }
 
     constexpr friend bool operator==(const StaticModInt& lhs,
-                                     const StaticModInt& rhs) {
+                                     const StaticModInt& rhs) noexcept {
         return lhs.v == rhs.v;
     }
 
     constexpr friend bool operator!=(const StaticModInt& lhs,
-                                     const StaticModInt& rhs) {
+                                     const StaticModInt& rhs) noexcept {
         return lhs.v != rhs.v;
     }
 
-    constexpr StaticModInt& operator+=(const StaticModInt& rhs) {
+    constexpr StaticModInt& operator+=(const StaticModInt& rhs) noexcept {
         v += rhs.val();
         if (M <= v) {
             v -= M;
@@ -38,7 +45,7 @@ template <unsigned M> class StaticModInt {
         return *this;
     }
 
-    constexpr StaticModInt& operator-=(const StaticModInt& rhs) {
+    constexpr StaticModInt& operator-=(const StaticModInt& rhs) noexcept {
         if (v < rhs.val()) {
             v += M;
         }
@@ -47,39 +54,43 @@ template <unsigned M> class StaticModInt {
         return *this;
     }
 
-    constexpr StaticModInt& operator*=(const StaticModInt& rhs) {
+    constexpr StaticModInt& operator*=(const StaticModInt& rhs) noexcept {
         v = static_cast<unsigned>(
             (static_cast<unsigned long long>(v) * rhs.val()) % M);
 
         return *this;
     }
 
-    constexpr StaticModInt& operator/=(const StaticModInt& rhs) {
+    constexpr StaticModInt& operator/=(const StaticModInt& rhs) noexcept {
+        assert(rhs.val() != 0);
+
         return *this *= rhs.inv();
     }
 
     constexpr friend StaticModInt operator+(const StaticModInt& lhs,
-                                            const StaticModInt& rhs) {
+                                            const StaticModInt& rhs) noexcept {
         return StaticModInt(lhs) += rhs;
     }
 
     constexpr friend StaticModInt operator-(const StaticModInt& lhs,
-                                            const StaticModInt& rhs) {
+                                            const StaticModInt& rhs) noexcept {
         return StaticModInt(lhs) -= rhs;
     }
 
     constexpr friend StaticModInt operator*(const StaticModInt& lhs,
-                                            const StaticModInt& rhs) {
+                                            const StaticModInt& rhs) noexcept {
         return StaticModInt(lhs) *= rhs;
     }
 
     constexpr friend StaticModInt operator/(const StaticModInt& lhs,
-                                            const StaticModInt& rhs) {
+                                            const StaticModInt& rhs) noexcept {
         return StaticModInt(lhs) /= rhs;
     }
 
-    constexpr StaticModInt pow(unsigned y) const {
-        StaticModInt res{1U}, x{*this};
+    constexpr StaticModInt pow(unsigned y) const noexcept {
+        StaticModInt res = raw(1);
+        StaticModInt x = *this;
+
         while (0 < y) {
             if (y & 1U) {
                 res *= x;
@@ -92,6 +103,6 @@ template <unsigned M> class StaticModInt {
         return res;
     }
 
-    constexpr StaticModInt inv() const { return pow(M - 2); }
+    constexpr StaticModInt inv() const noexcept { return pow(M - 2); }
 };
 };  // namespace Ku
