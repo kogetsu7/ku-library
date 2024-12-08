d99af0dc4c8d6e0e4a7209166170aa65699d22d3
kogetsu <kogetsu0728@gmail.com>
2024-12-08 15:33:42 +0900

    change: include順序

--- test/math/binomial.test.cpp
+++ test/math/binomial.test.cpp
@@ -1,26 +1,28 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/binomial_coefficient_prime_mod"
 
+#include "../../math/binomial.hpp"
+
 #include <algorithm>
 #include <iostream>
 
 #include <atcoder/modint>
 
-#include "../../math/binomial.hpp"
-
 int main() {
-    int T, M;
+  int T, M;
     std::cin >> T >> M;
 
-    atcoder::modint::set_mod(M);
+  atcoder::modint::set_mod(M);
 
-    ku::Binomial<atcoder::modint> bin(std::min(M - 1, 10000000));
+ku::Binomial<atcoder::modint> bin(std::min(M - 1, 10000000)
+    );
 
-    while (T--) {
+    while (T--)
+    {
         int n, k;
         std::cin >> n >> k;
 
         std::cout << bin.c(n, k).val() << "\n";
     }
 
-    return 0;
+        return 0;
 }
