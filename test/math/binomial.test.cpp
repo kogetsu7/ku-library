cdae1907ebd291d75af822989dd7b38e80ebe854
kogetsu7 <kogetsu7@users.noreply.github.com>
2024-12-08 06:34:06 +0000

    style: format

--- test/math/binomial.test.cpp
+++ test/math/binomial.test.cpp
@@ -8,21 +8,19 @@
 #include <atcoder/modint>
 
 int main() {
-  int T, M;
+    int T, M;
     std::cin >> T >> M;
 
-  atcoder::modint::set_mod(M);
+    atcoder::modint::set_mod(M);
 
-ku::Binomial<atcoder::modint> bin(std::min(M - 1, 10000000)
-    );
+    ku::Binomial<atcoder::modint> bin(std::min(M - 1, 10000000));
 
-    while (T--)
-    {
+    while (T--) {
         int n, k;
         std::cin >> n >> k;
 
         std::cout << bin.c(n, k).val() << "\n";
     }
 
-        return 0;
+    return 0;
 }
